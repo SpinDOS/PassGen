eea4e476aec9d72afd1c6e767b80db409920fe17
Alexander Platonov <alexanderplat757@gmail.com>
2024-07-07 17:55:05 +0300

    Simple control

--- PassGen.Maui/Control/ShowHidePasswordEntry.xaml.cs
+++ PassGen.Maui/Control/ShowHidePasswordEntry.xaml.cs
@@ -1,23 +1,31 @@
-namespace PassGen.Maui
+namespace PassGen.Maui;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ShowHidePasswordEntry : Grid
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ShowHidePasswordEntry : Grid
+    public static readonly BindableProperty HiddenProperty = BindableProperty.Create(nameof(Hidden), typeof(bool), typeof(ShowHidePasswordEntry), true);
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(ShowHidePasswordEntry));
+    public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(ShowHidePasswordEntry));
+
+    public ShowHidePasswordEntry() { InitializeComponent(); }
+
+    public bool Hidden
     {
-        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(ShowHidePasswordEntry));
-        public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(ShowHidePasswordEntry));
-
-        public ShowHidePasswordEntry() { InitializeComponent(); }
-
-        public string Text
-        {
-            get => (string) GetValue(TextProperty);
-            set => SetValue(TextProperty, value);
-        }
-        
-        public string Placeholder
-        {
-            get => (string) GetValue(PlaceholderProperty);
-            set => SetValue(PlaceholderProperty, value);
-        }
+        get => (bool) GetValue(HiddenProperty);
+        set => SetValue(HiddenProperty, value);
     }
+
+    public string Text
+    {
+        get => (string) GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+    public string Placeholder
+    {
+        get => (string) GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+
+    private void OnShowHideButtonClicked(object sender, EventArgs args) => Hidden = !Hidden;
 }
