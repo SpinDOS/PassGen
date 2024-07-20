a5da777223b90cf24a1bd94477a09881d824619a
Alexander Platonov <alexanderplat757@gmail.com>
2024-07-21 02:13:16 +0300

    Add unicode test

--- PassGen.Tests/BasicTests.cs
+++ PassGen.Tests/BasicTests.cs
@@ -12,5 +12,13 @@ namespace PassGen.Tests
             var generatedPassword = passwordGenerator.GeneratePassword("a", "b");
             Assert.AreEqual("p078064d5#7G", generatedPassword);
         }
+
+        [Test]
+        public void UnicodeTest()
+        {
+            var passwordGenerator = new PasswordGenerator();
+            var generatedPassword = passwordGenerator.GeneratePassword("а", "б");
+            Assert.AreEqual("p1e8cb032#7G", generatedPassword);
+        }
     }
 }
