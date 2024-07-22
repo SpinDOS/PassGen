172b9e331515e5f8ef8c8192efd5bda94e4f0c10
Alexander Platonov <alexanderplat757@gmail.com>
2024-07-22 11:37:12 +0300

    More verbosity

--- PassGen.Nginx/wwwroot/js/passgen.mjs
+++ PassGen.Nginx/wwwroot/js/passgen.mjs
@@ -1,7 +1,8 @@
-import "../lib/js-sha512/sha512.min.mjs";
+import "../lib/js-sha512/sha512.min.mjs"; // import sha512
 
 export default function generatePassword(targetSite, salt) {
     const stringToHash = targetSite + "@" + salt;
-    const sha512Result = sha512(stringToHash);
+    const utf8Bytes = new TextEncoder().encode(stringToHash);
+    const sha512Result = sha512.create().update(utf8Bytes).hex();
     return "p" + sha512Result.substring(0, 8) + "#7G";
 }
